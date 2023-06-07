e6ccc0681e3674368b4219d4a7f52b16e87533c6
ChiaraS97 <chiara.sucato97@gmail.com>
2023-06-07 17:45:33 +0200

    model type-project relazione

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Project extends Model
 {
     use HasFactory;
      protected $fillable = ['title', 'slug', 'image', 'description'];
+
+     public function type(): BelongsTo
+     {
+        return $this->belongsTo(Type::class);
+     }
 }
